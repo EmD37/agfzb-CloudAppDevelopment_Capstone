79a0336f7db07be5a850f5f3541dff76926ce2e8
EmD37 <davismike37@gmail.com>
2023-10-10 12:42:33 -0400

    Add Signup and Login to navbar

--- server/djangoapp/forms.py
+++ server/djangoapp/forms.py
@@ -3,7 +3,13 @@ from django.core.validators import EmailValidator
 
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100)
-    email = forms.CharField(validators=[EmailValidator()])
+    email = forms.CharField(widget=forms.EmailInput)
     phone = forms.CharField(max_length=15, required=False)
     subject = forms.CharField(max_length=100)
     message = forms.CharField(widget=forms.Textarea)
+
+class SignupForm(forms.Form):
+    email = forms.CharField(max_length=100, widget=forms.EmailInput)
+    username = forms.CharField(max_length=30)
+    password = forms.CharField(max_length=50, widget=forms.PasswordInput)
+    password_repeat = forms.CharField(max_length=50, widget=forms.PasswordInput)
